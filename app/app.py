979126d66d3dcc80e7af61d1b7c71006f0a595a7
DairanDoo <your_email@example.com>
2024-11-01 21:49:27 -0400

    ...

--- app/app.py
+++ app/app.py
@@ -1,11 +1,17 @@
-from flask import Flask
+from flask import Flask, jsonify
+
+import datetime
 
 def create_app():
     app = Flask(__name__)
+
     
     # Ruta simple que retorna un mensaje
     @app.route('/')
     def home():
         return "Hola Mundo ( Ruta De Testing )"
+    
+    
+        
     
     return app
