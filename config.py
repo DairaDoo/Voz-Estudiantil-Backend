5e715409915d10a98f652439a4f9df79bb8bc3d9
Ian <CorreoIanembuste@gmail.com>
2024-11-07 16:32:31 -0400

    Cree la conexion a la base de datos y agrege la url de la base de datos.

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 import os
 
 class Config:
-    pass
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://voz_estudiantil_db_user:egsVv9dWWVJVhj4GxiXfth5kElhQ0TPe@dpg-csmfeb56l47c73eqvvd0-a/voz_estudiantil_db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     
