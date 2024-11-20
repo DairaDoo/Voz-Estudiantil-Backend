b84a59871a53e470e2486b90dd13cdc0a3f7c654
lvctech <csjblcordero@gmail.com>
2024-11-20 13:35:46 -0400

    Error con flask-cors que no permite peticiones de otros puertos solucionado

--- app/app.py
+++ app/app.py
@@ -1,9 +1,13 @@
 from flask import Flask
 from config import Config
 from app.routes.user_routes import user_routes  # Registra el Blueprint de las rutas de usuario
+from flask_cors import CORS
 
 # Inicializa la aplicación Flask
 app = Flask(__name__)
+
+# Habilitar CORS para todas las rutas
+CORS(app)
 
 # Configura la aplicación con variables del archivo de configuración
 app.config.from_object(Config)
