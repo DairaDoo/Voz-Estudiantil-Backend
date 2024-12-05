d69e8acf21ce4052031b0e391a997e55d07ddf2d
Ian <CorreoIanembuste@gmail.com>
2024-12-05 00:49:50 -0400

    Cree la ruta que devuelve eventos.

---
+++ app/routes/event_routes.py
@@ -0,0 +1,7 @@
+from flask import Blueprint
+from controllers.event_controller import get_all_events
+
+event_routes = Blueprint('event_routes', __name__)
+
+# Definir ruta GET /events
+event_routes.route('/events', methods=['GET'])(get_all_events)
